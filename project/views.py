0d293d0420f1ad8e7e5de42c08939989bb6cd2b6
Patrick Kimber <code@pkimber.net>
2013-10-26 17:14:50 +0100

    update template view so we can use 'hatherleigh.net' templates

--- project/views.py
+++ project/views.py
@@ -1,8 +1,18 @@
 from django.views.generic import TemplateView
 
+from story.models import Story
+
 
 class HomeView(TemplateView):
-    template_name = 'project/home.html'
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context.update(dict(
+            #story_list=Story.objects.filter(date_moderated__isnull=False),
+            story_list=Story.objects.all(),
+        ))
+        return context
 
 
 class SecureView(TemplateView):
