62d67892eb3d3cde3279dd1c4a1d71007676a1d8
Patrick Kimber <code@pkimber.net>
2013-12-16 17:19:50 +0100

    version 0.0.32

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
             'templates/project/*.*',
         ],
     },
-    version='0.0.31',
+    version='0.0.32',
     description='hatherleigh web site',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
