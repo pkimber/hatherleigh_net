c85e101acfd224a766c82a78000bf3c02287b0bd
Patrick Kimber <code@pkimber.net>
2014-02-05 17:35:08 +0000

    event forms and move bleach clean to a function in the 'base' app

--- pump/forms.py
+++ pump/forms.py
@@ -1,18 +1,89 @@
 from django import forms
 
-from bleach import (
-    clean,
-    ALLOWED_TAGS,
-    ALLOWED_ATTRIBUTES,
-)
 from captcha.fields import CaptchaField
 
 from base.form_utils import (
+    bleach_clean,
     RequiredFieldForm,
 )
 
-from .models import Story
+from .models import (
+    Event,
+    Story,
+)
 
+
+class EventEmptyForm(forms.ModelForm):
+
+    class Meta:
+        model = Event
+        fields = ()
+
+
+class EventForm(RequiredFieldForm):
+    """This is an 'abstract' base class, designed to inherited."""
+
+    def __init__(self, *args, **kwargs):
+        super(EventForm, self).__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs.update(
+            {'class': 'pure-input-2-3'}
+        )
+        self.fields['description'].widget.attrs.update(
+            {'class': 'pure-input-2-3'}
+        )
+
+    def clean_description(self):
+        data = self.cleaned_data['description']
+        return bleach_clean(data)
+
+
+class EventAnonForm(EventForm):
+    """user is not logged in... so we need a captcha"""
+    captcha = CaptchaField()
+
+    def __init__(self, *args, **kwargs):
+        super(EventAnonForm, self).__init__(*args, **kwargs)
+        self.fields['email'].required = True
+        self.fields['name'].required = True
+        self.fields['name'].widget.attrs.update(
+            {'class': 'pure-input-1-2'}
+        )
+        self.fields['email'].widget.attrs.update(
+            {'class': 'pure-input-1-2'}
+        )
+
+    class Meta:
+        model = Event
+        fields = (
+            'name',
+            'email',
+            'area',
+            'event_date',
+            'event_time',
+            'title',
+            'description',
+            'picture'
+        )
+        widgets = {
+            'picture': forms.FileInput,
+        }
+
+
+class EventTrustForm(EventForm):
+
+    class Meta:
+        model = Event
+        fields = (
+            'area',
+            'event_date',
+            'event_time',
+            'title',
+            'description',
+            'picture',
+        )
+        widgets = {
+            'picture': forms.FileInput,
+        }
 
 class StoryEmptyForm(forms.ModelForm):
 
@@ -35,15 +106,7 @@ class StoryForm(RequiredFieldForm):
 
     def clean_description(self):
         data = self.cleaned_data['description']
-        #attributes = ALLOWED_ATTRIBUTES
-        #attributes['img'] = ['align', 'alt', 'src', 'style']
-        #styles = ['display', 'height', 'width',]
-        #tags = ALLOWED_TAGS + [u'img',]
-        attributes = ALLOWED_ATTRIBUTES
-        styles = []
-        tags = ALLOWED_TAGS + [u'br', u'p',]
-        data = clean(data, tags=tags, attributes=attributes, styles=styles)
-        return data
+        return bleach_clean(data)
 
 
 class StoryAnonForm(StoryForm):
