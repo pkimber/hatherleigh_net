c18c428dc1058a61f3b949e7b6c0eaf5ea39954c
Patrick Kimber <code@pkimber.net>
2014-02-05 16:52:53 +0000

    events - move common fields from story to an abstract class

--- pump/models.py
+++ pump/models.py
@@ -32,49 +32,18 @@ class Area(models.Model):
 reversion.register(Area)
 
 
-class Event(ContentModel):
-    """ Event """
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
-    email = models.EmailField(blank=True, null=True)
-    name = models.CharField(max_length=100, blank=True)
-    area = models.ForeignKey(Area)
-    title = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
+class PumpContentModel(ContentModel):
 
-    class Meta:
-        ordering = ['modified']
-        verbose_name = 'Event'
-        verbose_name_plural = 'Events'
-
-    def __unicode__(self):
-        return unicode('{}'.format(self.title))
-
-reversion.register(Event)
-
-
-class Story(ContentModel):
-    """News story"""
     user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
     name = models.CharField(max_length=100, blank=True)
     area = models.ForeignKey(Area)
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    picture = models.ImageField(upload_to='story/%Y/%m/%d', blank=True)
+    picture = models.ImageField(upload_to='pump/%Y/%m/%d', blank=True)
 
     class Meta:
-        # cannot put 'unique_together' on abstract base class
-        # https://code.djangoproject.com/ticket/16732
-        unique_together = ('container', 'moderate_state')
-        ordering = ['-created']
-        verbose_name = 'Story'
-        verbose_name_plural = 'Stories'
-
-    def _get_content_set(self):
-        return self.container.story_set
-
-    def __unicode__(self):
-        return unicode('{}'.format(self.title))
+        abstract = True
 
     def save(self, *args, **kwargs):
         if self.user:
@@ -85,10 +54,7 @@ class Story(ContentModel):
             raise ValueError(
                 "Story must have a 'user' or a 'name' AND 'email'"
             )
-        super(Story, self).save(*args, **kwargs)
-
-    def get_absolute_url(self):
-        return reverse('pump.story.detail', args=[self.pk])
+        super(PumpContentModel, self).save(*args, **kwargs)
 
     def user_can_edit(self, user):
         """
@@ -105,5 +71,43 @@ class Story(ContentModel):
     def _author(self):
         return self.name or self.user.username
     author = property(_author)
+
+
+class Event(PumpContentModel):
+    """Event."""
+    event_date = models.DateField()
+    event_time = models.TimeField()
+
+    class Meta:
+        ordering = ['modified']
+        verbose_name = 'Event'
+        verbose_name_plural = 'Events'
+
+    def __unicode__(self):
+        return unicode('{}'.format(self.title))
+
+reversion.register(Event)
+
+
+class Story(PumpContentModel):
+    """News story"""
+    pass
+
+    class Meta:
+        # cannot put 'unique_together' on abstract base class
+        # https://code.djangoproject.com/ticket/16732
+        unique_together = ('container', 'moderate_state')
+        ordering = ['-created']
+        verbose_name = 'Story'
+        verbose_name_plural = 'Stories'
+
+    def _get_content_set(self):
+        return self.container.story_set
+
+    def __unicode__(self):
+        return unicode('{}'.format(self.title))
+
+    def get_absolute_url(self):
+        return reverse('pump.story.detail', args=[self.pk])
 
 reversion.register(Story)
