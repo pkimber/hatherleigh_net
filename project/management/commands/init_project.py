c1b988eb03636a46103c517086b24192478760b0
Patrick Kimber <code@pkimber.net>
2014-03-19 17:15:00 +0000

    init_project

--- project/management/commands/init_project.py
+++ project/management/commands/init_project.py
@@ -1,11 +1,14 @@
 # -*- encoding: utf-8 -*-
-
 from __future__ import unicode_literals
+
 """
 This command is designed to be run multiple times.  It will clear out data, and
 then re-insert e.g. for setting up the main menu navigation.
 """
 from django.core.management.base import BaseCommand
+
+from block.tests.scenario import default_moderate_state
+from pump.tests.scenario import default_section
 
 
 class Command(BaseCommand):
@@ -13,4 +16,6 @@ class Command(BaseCommand):
     help = "Set-up project (e.g. main navigation)"
 
     def handle(self, *args, **options):
+        default_section()
+        default_moderate_state()
         print("Project initialised...")
--- pump/tests/test_management_command.py
+++ pump/tests/test_management_command.py
@@ -1,12 +1,11 @@
 # -*- encoding: utf-8 -*-
-
 from __future__ import unicode_literals
+
 from django.test import TestCase
 
 from login.management.commands import demo_data_login
 
 from pump.management.commands import demo_data_pump
-from pump.management.commands import init_app_pump
 
 
 class TestCommand(TestCase):
@@ -16,9 +15,4 @@ class TestCommand(TestCase):
         pre_command = demo_data_login.Command()
         pre_command.handle()
         command = demo_data_pump.Command()
-        command.handle()
-
-    def test_init_app(self):
-        """ Test the management command """
-        command = init_app_pump.Command()
         command.handle()
