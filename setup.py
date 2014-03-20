9c100a2bda7f55ace04be89b85f39bff1ac20d43
Patrick Kimber <code@pkimber.net>
2014-03-20 13:50:48 +0100

    version 0.0.42

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
             'templates/pump/*.*',
         ],
     },
-    version='0.0.41',
+    version='0.0.42',
     description='hatherleigh web site',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
